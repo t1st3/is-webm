ea0696d91ef8e2a75b8f2a335d128317b70569fc
t1st3 <contact@tiste.org>
2017-04-11 17:09:11 +0200

    feat: (breaking) es2015ify and drop node 0.10 and 0.12
    * Bump deps
    * drop support for Node.js 0.10 and 0.12
    * use ES2015

--- test.js
+++ test.js
@@ -3,16 +3,10 @@ import readChunk from 'read-chunk';
 import test from 'ava';
 import isWebm from './';
 
-test('should detect WEBM from buffer', function (t) {
-	readChunk(path.join(__dirname, 'fixture.webm'), 0, 4, function (err, buf) {
-		t.is(err, null);
-		t.is(isWebm(buf), true);
-	});
+test('should detect WEBM from buffer', t => {
+	t.is(isWebm(readChunk.sync(path.join(__dirname, 'fixture.webm'), 0, 4)), true);
 });
 
-test('should not detect WEBM on a non-WEBM file', function (t) {
-	readChunk(path.join(__dirname, 'fixture.jpg'), 0, 4, function (err, buf) {
-		t.is(err, null);
-		t.is(isWebm(buf), false);
-	});
+test('should not detect WEBM on a non-WEBM file', t => {
+	t.is(isWebm(readChunk.sync(path.join(__dirname, 'fixture.jpg'), 0, 4)), false);
 });
